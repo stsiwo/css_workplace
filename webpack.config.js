b55e32543e298d2f9f30adac63c4d19dcf8bdf8e
Satoshi Iwao <stsiwo@gmail.com>
2018-10-10 22:51:37 -0700

    configured file loader in webpack

--- webpack.config.js
+++ webpack.config.js
@@ -25,6 +25,12 @@ module.exports = {
             loader: 'postcss-loader'
           }
         ]
+      },
+      {
+        test: /\.(png|svg|jpg|gif)$/,
+          use: [
+            'file-loader'
+          ]
       }
     ]
   }
