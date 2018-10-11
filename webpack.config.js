2cf3edde7c449d4f84735b89bb56063938925a1d
Satoshi Iwao <stsiwo@gmail.com>
2018-10-11 00:19:03 -0700

    configured font loader

--- webpack.config.js
+++ webpack.config.js
@@ -31,7 +31,14 @@ module.exports = {
           use: [
             'file-loader'
           ]
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/,
+        use: [
+          'file-loader'
+        ]
       }
+      
     ]
   }
 };
