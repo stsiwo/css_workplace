2e4cd5ad0cd6151f877f0e81d293f081a5af394f
Satoshi Iwao <stsiwo@gmail.com>
2018-10-11 03:09:49 -0700

    configured html webpack plugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,17 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   entry: {
     app: './src/index.js',
     print: './src/print.js'
   },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'Output Management',
+      meta: {viewport: 'width=device-width, initial-scale=1, shrink-to-fit=no'}
+    })
+  ],
   output: {
     filename: '[name].bundle.js',
     path: path.resolve(__dirname, 'dist')
