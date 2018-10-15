479444a3fbd47b91b6aea28d81d617e12b3191f0
Satoshi Iwao <stsiwo@gmail.com>
2018-10-15 12:52:10 -0700

    update: add placeholder to input

--- src/components/searchBox/SearchBox.jsx
+++ src/components/searchBox/SearchBox.jsx
@@ -7,7 +7,7 @@ class SearchBox extends React.Component {
   render() {
     return (
       <div className={ styles.searchBox }>
-        <input type="text" className={ styles.searchInput }/>
+        <input type="text" className={ styles.searchInput } placeholder="search keys ..."/>
         <img className={ styles.searchIcon } src={searchIconSvg} />
       </div>
      ); 
