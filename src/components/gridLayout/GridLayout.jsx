b34a1705ce34164ca0b1bffde0893ad40eca46e1
Satoshi Iwao <stsiwo@gmail.com>
2018-10-17 11:03:10 -0700

    implemented grid layout component

--- src/components/gridLayout/GridLayout.jsx
+++ src/components/gridLayout/GridLayout.jsx
@@ -1,16 +1,24 @@
 import React from 'react';
 import styles from './gridLayout.css';
+import items from './itemFactory';
+
 
 class GridLayout extends React.Component {
+  constructor(props) {
+    super(props);
+    this.renderItems = this.renderItems.bind(this);
+  }
+  renderItems(items) {
+    return items.map( (item) =>  
+      <div key={ item.id } className={ styles.node }>
+        <img src={item.img} className={ styles.img } /> 
+      </div>
+    );
+  }
   render() {
     return (
       <div className={ styles.gridContainer }>
-        <div id={ styles.node1 } className={ styles.node }>node 1</div>
-        <div id={ styles.node2 } className={ styles.node }>node 2</div>
-        <div id={ styles.node3 } className={ styles.node }>node 3</div>
-        <div id={ styles.node4 } className={ styles.node }>node 4</div>
-        <div id={ styles.node5 } className={ styles.node }>node 5</div>
-        <div id={ styles.node6 } className={ styles.node }>node 6</div>
+        { this.renderItems(items) }
       </div>
      ); 
   }
---
+++ src/components/gridLayout/itemFactory.js
@@ -0,0 +1,51 @@
+import images from './imgExporter';
+
+const items = [
+  {
+    id: 1,
+    img: images[0],
+    content: null
+  },
+  {
+    id: 2,
+    img: images[1],
+    content: null
+  },
+  {
+    id: 3,
+    img: images[2],
+    content: null
+  },
+  {
+    id: 4,
+    img: images[3],
+    content: null
+  },
+  {
+    id: 5,
+    img: images[4],
+    content: null
+  },
+  {
+    id: 6,
+    img: images[5],
+    content: null
+  },
+  {
+    id: 7,
+    img: images[6],
+    content: null
+  },
+  {
+    id: 8,
+    img: images[7],
+    content: null
+  },
+  {
+    id: 9,
+    img: images[8],
+    content: null
+  },
+];
+
+export default items;
